f5c71c94bb457edcb95ef7792942dbda1801a296
Zackery Wilson <zackazt@gmail.com>
2016-05-10 19:31:59 -0400

    Adding static files, beginning design, images can now be posted.

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,18 @@
-from django.http import HttpResponse
-
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 def post_create(request):
-	form = PostForm()
+	form = PostForm(request.POST or None, request.FILES or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		messages.success(request, "Successfully created!")
+		return HttpResponseRedirect(instance.get_absolute_url())
 	context = {
 		"form": form,
 	}
@@ -16,20 +22,46 @@ def post_detail(request, id=None):
 	instance = get_object_or_404(Post, id=id)
 	context = {
 		"title": instance.title,
-		"instance": instance
+		"instance": instance,
 	}
 	return render(request, "post_detail.html", context)
 
 def post_list(request):
-	queryset = Post.objects.all()
+	queryset_list= Post.objects.all().order_by("-updated")
+	paginator = Paginator(queryset_list, 10) # Show 25 contacts per page
+	page = request.GET.get('page')
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
 	context = {
-	    "object_list": queryset,
+		"object_list": queryset,
 		"title": "List"
 	}
-	return render(request, "index.html", context)
+	return render(request, "post_list.html", context)
 
-def post_update(request):
-	return HttpResponse("<h1>Hello World: Update!</h1>")
+def post_update(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		messages.success(request, "Successfully updated!")
+		return HttpResponseRedirect(instance.get_absolute_url())
+	context = {
+		"title": instance.title,
+		"instance": instance,
+		"form": form,
+	}
+	return render(request, "post_form.html", context)
 
-def post_delete(request):
-	return HttpResponse("<h1>Hello World: Delete!</h1>")
+
+def post_delete(request, id=id):
+	instance = get_object_or_404(Post, id=id)
+	instance.delete()
+	messages.success(request, "Successfully deleted!")
+	return redirect("posts:list")
