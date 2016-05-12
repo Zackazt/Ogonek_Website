b66eafe8881d21dd067ef3124bc82930adc60074
Zackery Wilson <zackazt@gmail.com>
2016-05-12 13:00:31 -0400

    Chose a color palett.  Implemented slug for urls.  The most recently updated post now has it's own DIV section at the top of the Article Feed page.

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 # Create your models here.
+
+def upload_location(instance, filename):
+	return "%s/%s" %(instance.id, filename)
+
 class Post(models.Model):
 	title = models.CharField(max_length=120)
-	image = models.FileField(null=True)
+	slug = models.SlugField(unique=True)
+	image = models.ImageField(
+		upload_to=upload_location,
+		null=True, 
+		width_field="width_field", 
+		height_field="height_field"
+	)
+	height_field = models.IntegerField(default=0)
+	width_field = models.IntegerField(default=0)
 	body = models.TextField(max_length=20000)
 	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
@@ -13,4 +27,22 @@ class Post(models.Model):
 		return self.title
 
 	def get_absolute_url(self):
-		return reverse("posts:detail", kwargs={"id": self.id})
+		return reverse("posts:detail", kwargs={"slug": self.slug})
+
+def create_slug(instance, new_slug=None):
+	slug = slugify(instance.title)
+	if new_slug != None:
+		slug = new_slug
+	qs = Post.objects.filter(slug=slug).order_by("-id")
+	exists = qs.exists()
+	if exists:
+		 new_slug = "%s-%s" %(slug, qs.first().id)
+		 return create_slug(instance, new_slug=new_slug)
+	return slug
+
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+	if not instance.slug:
+		instance.slug = create_slug(instance)
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
