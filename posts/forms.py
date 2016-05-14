b743ce08c927718861664fa6b6da9008c5bc5bc5
Zackery Wilson <zackazt@gmail.com>
2016-05-13 20:58:30 -0400

    Added a genre field for every post.

--- posts/forms.py
+++ posts/forms.py
@@ -2,8 +2,21 @@ from django import forms
 from .models import Post
 
 
+
 class PostForm(forms.ModelForm):
+	genre_list = (
+			('Opinion', 'Opinion'), 
+			('Lifestyle', 'Lifestyle'),
+			('History', 'History'),
+			('Health', 'Health'),
+			('Technology', 'Technology'),
+			('Science', 'Science'),
+			('Music', 'Music'),
+			('Art', 'Art'),
+			('Social', 'Social'),
+		)
 	publish = forms.DateField(widget=forms.SelectDateWidget)
+	genre = forms.ChoiceField(choices=genre_list)
 	class Meta:
 		model = Post
 		fields = [
@@ -12,4 +25,5 @@ class PostForm(forms.ModelForm):
 			"body",
 			"draft",
 			"publish",
+			"genre",
 		]
--- posts/models.py
+++ posts/models.py
@@ -3,12 +3,16 @@ from django.core.urlresolvers import reverse
 from django.db.models.signals import pre_save
 from django.utils.text import slugify
 from django.conf import settings
+from django import forms
 # Create your models here.
 
 def upload_location(instance, filename):
 	return "%s/%s" %(instance.id, filename)
 
+
 class Post(models.Model):
+	# genres = ["Lifestyle", "Health", "Politics", "Music", "History", "Opinion" "News"]
+	genre= models.CharField(max_length=25, default="Opinion")
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
 	title = models.CharField(max_length=120)
 	slug = models.SlugField(unique=True)
@@ -25,7 +29,6 @@ class Post(models.Model):
 	publish = models.DateField(auto_now=False, auto_now_add=False)
 	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
-
 
 	def __str__(self):
 		return self.title
